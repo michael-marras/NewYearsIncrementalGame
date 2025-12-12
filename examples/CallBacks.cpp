0bca0b9f6e77c2159d23a9fe3597f3734006a901
Michael Marras <michaelmarras@Michaels-MacBook-Air-6.local>
2025-12-12 13:53:13 -0800

    Created an examples directory to save my work while learning SDL3

---
+++ examples/CallBacks.cpp
@@ -0,0 +1,29 @@
+#define SDL_MAIN_USE_CALLBACKS 1
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+SDL_Window* window;
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
+    if(!SDL_Init(SDL_INIT_VIDEO)) {
+        return SDL_APP_FAILURE;
+    }
+
+    window = SDL_CreateWindow("pp", 320, 240, SDL_WINDOW_RESIZABLE);
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate) {
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+    SDL_Quit();
+}
+
+
